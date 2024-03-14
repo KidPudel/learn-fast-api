d915a8560c2e79ce5421bdb19293a9750364a3d9
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-03-14 14:44:41 +0300

    ignoring venv

--- api/get_wishes.py
+++ api/get_wishes.py
@@ -7,7 +7,7 @@ async def get_wishes_from_db(amount: int) -> list[Any]:
     async with database.pgpool.connection() as conn:
         async with conn.cursor() as acursor:
             await acursor.execute(
-                "select * from wishes limit %(amount)s;", {"amount": amount}
+                "select * from wishes limit %(amount)s;", {"amount": amount}, True
             )
             wishes = await acursor.fetchall()
             return wishes
